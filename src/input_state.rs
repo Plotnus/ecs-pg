5f0270f3ea6576979bd5550c8d21afb43a4ee31c
Jonathan Plotner <plotnus@gmail.com>
2019-05-25 13:17:04 -0700

    got basic input for two sticks set up: + normalized direction + inner dead zone + outer dead zone + types for raw, core, and game input

--- src/input_state.rs
+++ src/input_state.rs
@@ -15,3 +15,93 @@ impl InputState {
         }
     }
 }
+#[derive(Debug)]
+pub struct RawControllerInput {
+    pub ls_x: i16,
+    pub ls_y: i16,
+    pub rs_x: i16,
+    pub rs_y: i16,
+}
+/// this is just a copy of the input state for the controller
+/// it does not do float representations or any reformatting
+// one concern is what happens when a button goes down and up on one frame
+// for our purposes we won't have to address that
+impl RawControllerInput {
+    pub fn new() -> RawControllerInput {
+        RawControllerInput {
+            ls_x: 0,
+            ls_y: 0,
+            rs_x: 0,
+            rs_y: 0,
+        }
+    }
+    pub fn from_sdl2_controller(
+        controller: &sdl2::controller::GameController,
+    ) -> RawControllerInput {
+        RawControllerInput {
+            ls_x: controller.axis(sdl2::controller::Axis::LeftX),
+            ls_y: controller.axis(sdl2::controller::Axis::LeftY),
+            rs_x: controller.axis(sdl2::controller::Axis::RightX),
+            rs_y: controller.axis(sdl2::controller::Axis::RightY),
+        }
+    }
+}
+/// this is created from a RawControllerInputState.
+pub struct CoreControllerInput {
+    pub ls: Vec2,
+    pub rs: Vec2,
+}
+
+impl CoreControllerInput {
+    pub fn from_raw(raw: &RawControllerInput) -> CoreControllerInput {
+        // range from [-1,1] with y-axis flipped so up is (+) and down is (-)
+        let ls = Vec2::new(normalize_i16(raw.ls_x), -normalize_i16(raw.ls_y));
+        let rs = Vec2::new(normalize_i16(raw.rs_x), -normalize_i16(raw.rs_y));
+
+        // clamp magnitude between [0,1]
+        let ls_mag = ls.magnitude().clamp(0.0, 1.0);
+        let rs_mag = rs.magnitude().clamp(0.0, 1.0);
+
+        // create so input is between 0 and 1
+        CoreControllerInput {
+            ls: ls.normalized().scaled(ls_mag),
+            rs: rs.normalized().scaled(rs_mag),
+        }
+    }
+}
+fn normalize_i16(x: i16) -> f32 {
+    if x < 0 {
+        x as f32 / std::i16::MIN as f32
+    } else {
+        x as f32 / std::i16::MAX as f32
+    }
+}
+
+///
+#[derive(Debug)]
+pub struct GameControllerInput {
+    pub ls: Vec2,
+    pub rs: Vec2,
+}
+impl GameControllerInput {
+    /*
+    pub fn new() -> GameControllerInput {
+        GameControllerInput {
+            ls: Vec2::new(0.0, 0.0),
+            rs: Vec2::new(0.0, 0.0),
+        }
+    }
+    */
+    pub fn from_core_and_transform<F>(
+        core_controller_input: &CoreControllerInput,
+        stick_transform: F,
+    ) -> GameControllerInput
+    where
+        F: Fn(Vec2) -> Vec2,
+    {
+        GameControllerInput {
+            ls: stick_transform(core_controller_input.ls),
+            rs: stick_transform(core_controller_input.rs),
+        }
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -10,9 +10,11 @@ mod projectile_state;
 mod tile;
 
 use crate::game_state::GameState;
-use crate::input_state::InputState;
 
 // used for input
+use input_state::CoreControllerInput;
+use input_state::GameControllerInput;
+use input_state::RawControllerInput;
 use math::Vec2;
 
 fn main() -> Result<(), String> {
@@ -22,21 +24,21 @@ fn main() -> Result<(), String> {
     let sdl_context = sdl2::init()?;
 
     // initialize controller - taken from rust-sdl2 example
-    let game_controller_subsystem = sdl_context.game_controller()?;
-    let num_joysticks_available = game_controller_subsystem
+    let controller_subsystem = sdl_context.game_controller()?;
+    let num_joysticks_available = controller_subsystem
         .num_joysticks()
         .map_err(|e| format!("can't enumerate joysticks: {}", e))?;
-    // Iterate over all available joysticks and look for game controllers.
-    let mut controller = (0..num_joysticks_available)
+    // Iterate over all available joysticks and look for a game controller.
+    let controller = (0..num_joysticks_available)
         .find_map(|id| {
-            if !game_controller_subsystem.is_game_controller(id) {
+            if !controller_subsystem.is_game_controller(id) {
                 println!("{} is not a game controller", id);
                 return None;
             }
 
             println!("Attempting to open controller {}", id);
 
-            match game_controller_subsystem.open(id) {
+            match controller_subsystem.open(id) {
                 Ok(c) => {
                     // We managed to find and open a game controller,
                     // exit the loop
@@ -51,81 +53,34 @@ fn main() -> Result<(), String> {
         })
         .expect("Couldn't open any controller");
 
-    // initialize window
-    // initialize context
-
     let mut game_state = GameState::new();
 
-    let mut input_state = InputState::new();
     loop {
-        for event in sdl_context.event_pump().unwrap().poll_iter() {
-            use sdl2::controller::Axis;
-            use sdl2::event::Event;
-            if let Event::ControllerAxisMotion {
-                axis,
-                value: raw_val,
-                ..
-            } = event
-            {
-                // linear mapping of raw_val to the range [-1,1]
-                let t = if raw_val >= 0 {
-                    (raw_val as f32 / std::i16::MAX as f32)
+        let game_controller_input = {
+            // because we are not using the event loop we must call update on the controller subsystem
+            controller_subsystem.update();
+            let raw_controller_input = RawControllerInput::from_sdl2_controller(&controller);
+            let core_controller_input = CoreControllerInput::from_raw(&raw_controller_input);
+
+            const INNER_DEAD_ZONE: f32 = 0.25;
+            const OUTER_DEAD_ZONE: f32 = 0.75;
+            let axis_transform = |v: Vec2| -> Vec2 {
+                let mag = v.magnitude();
+                let mag = if mag < INNER_DEAD_ZONE {
+                    0.0
+                } else if mag > OUTER_DEAD_ZONE {
+                    1.0
                 } else {
-                    -(raw_val as f32 / std::i16::MIN as f32)
+                    (mag - INNER_DEAD_ZONE) / (OUTER_DEAD_ZONE - INNER_DEAD_ZONE)
                 };
-
-                // assign t
-                if axis == Axis::LeftX {
-                    input_state.ls.x = t;
-                } else if axis == Axis::LeftY {
-                    input_state.ls.y = t;
-                } else if axis == Axis::RightX {
-                    input_state.rs.x = t;
-                } else if axis == Axis::RightY {
-                    input_state.rs.y = t;
-                }
-
-                // flip y value so up is (+) and down is (-)
-                if axis == Axis::LeftY {
-                    input_state.ls.y = -input_state.ls.y;
-                } else if axis == Axis::RightY {
-                    input_state.rs.y = -input_state.rs.y;
-                }
-
-                // calculate magnitude and direction for input
-                let mag = input_state.ls.magnitude();
-                let mag = mag.clamp(0.0, 1.0);
-                let dir = if mag != 0.0 {
-                    input_state.ls.normalized()
-                } else {
-                    Vec2::zero()
-                };
-
-                // map magnitude to account for dead zones
-                // this step is really a game design step
-                // for our purposes this will mostly be 0 or 1
-                // to have that max snappy feeling of twin stick...
-                // this may be bad depending on if players want the ability
-                // to have finer control over the character
-                let f = |x: f32| -> f32 {
-                    if x < 0.2 {
-                        // inner dead zone
-                        0.0
-                    } else {
-                        // outer deadzone is anything <= 0.2
-                        1.0
-                    }
-                };
-
-                // apply magnitude to direction
-                input_state.ls = dir.scaled(f(mag));
-            }
-        }
-        println!("input: {:?}", input_state);
-        //let input_state = capture_input_state();
+                return v.normalized().scaled(mag);
+            };
+            GameControllerInput::from_core_and_transform(&core_controller_input, axis_transform)
+        };
+        println!("input: {:?}", game_controller_input);
 
         // modify the game state based on the input
-        game_state = input_system(game_state, &input_state);
+        //    game_state = input_system(game_state, &input_state);
 
         // we want physics first because we want the world to be in a valid state before
         // running the entity logic.
@@ -143,11 +98,25 @@ fn main() -> Result<(), String> {
     Ok(())
 }
 
-pub fn capture_input_state() -> InputState {
-    InputState::new()
+pub fn update_raw_input(raw_input: &mut RawControllerInput, sdl_ctx: &sdl2::Sdl) {
+    for event in sdl_ctx.event_pump().unwrap().poll_iter() {
+        if let sdl2::event::Event::ControllerAxisMotion {
+            axis, value: val, ..
+        } = event
+        {
+            match axis {
+                sdl2::controller::Axis::LeftX => raw_input.ls_x = val,
+                sdl2::controller::Axis::LeftY => raw_input.ls_y = val,
+                sdl2::controller::Axis::RightX => raw_input.rs_x = val,
+                sdl2::controller::Axis::RightY => raw_input.rs_y = val,
+                sdl2::controller::Axis::TriggerLeft => (),
+                sdl2::controller::Axis::TriggerRight => (),
+            }
+        }
+    }
 }
 
-pub fn input_system(game_state: GameState, _input_state: &InputState) -> GameState {
+pub fn input_system(game_state: GameState, _input_state: &GameControllerInput) -> GameState {
     // println!("input_system");
     game_state
 }
